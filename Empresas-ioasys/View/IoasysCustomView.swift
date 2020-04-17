7314dd4fc58c6d2d6e3be0b6e45251ba2119057d
João Victor Moreira Tribuzy <jvtribuzy@gmail.com>
2020-04-17 00:37:31 -0400

    Splashscreen

---
+++ Empresas-ioasys/View/IoasysCustomView.swift
@@ -0,0 +1,14 @@
+//
+//  IoasysCustomView.swift
+//  Empresas-ioasys
+//
+//  Created by João Tribuzy on 17/04/20.
+//  Copyright © 2020 João Tribuzy. All rights reserved.
+//
+
+import Foundation
+
+protocol IoasyCustomView {
+    func style()
+    func autolayout()
+}
