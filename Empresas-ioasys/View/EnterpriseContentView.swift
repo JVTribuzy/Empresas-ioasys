2329dac5fc0d22d4b00937cbd7bb599a18f269f2
João Victor Moreira Tribuzy <jvtribuzy@gmail.com>
2020-04-19 10:00:41 -0400

    Showing empty indication when Table view is empty

--- Empresas-ioasys/View/EnterpriseContentView.swift
+++ Empresas-ioasys/View/EnterpriseContentView.swift
@@ -11,11 +11,16 @@ import UIKit
 import Stevia
 
 class EnterpriseContentView: UIView{
+    var model: EnterpriseModelController = {
+        return EnterpriseModelController.shared
+    }()
     
     private override init(frame: CGRect) {
         super.init(frame: .zero)
         autolayout()
         style()
+        
+        addNotification()
     }
     
     required init?(coder: NSCoder) {
@@ -23,15 +28,59 @@ class EnterpriseContentView: UIView{
     }
     
     private let enterpriseTableViewController: UITableViewController = EnterpriseTableViewController(style: .plain)
+    public let enterpriseQuantity: UILabel = UILabel()
+    private let unknownResult: UILabel = UILabel()
 }
 
 extension EnterpriseContentView: IoasyCustomView{
     func autolayout() {
         // enterpriseTableViewController
-        subviews(enterpriseTableViewController.view)
+        subviews(enterpriseTableViewController.view, unknownResult, enterpriseQuantity)
         enterpriseTableViewController.view.left(0).top(80).right(0).bottom(0)
+        
+        unknownResult.right(16).left(16).height(40).top(180)
+        
+        enterpriseQuantity.Top == self.Top + 40
+        enterpriseQuantity.right(16).left(16)
     }
     
     func style() {
+        
+        unknownResult.isHidden = true
+        unknownResult.numberOfLines = 1
+        unknownResult.textAlignment = .center
+        unknownResult.font = UIFont(name: "Rubik-Regular", size: 18)
+        unknownResult.textColor = UIColor.ioasysUnknownResulTextColor
+        unknownResult.text = NSLocalizedString("Nenhum resultado encontrado", comment: "")
+        
+        enterpriseQuantity.numberOfLines = 1
+        enterpriseQuantity.textAlignment = .left
+        enterpriseQuantity.font = UIFont(name: "Rubik-Regular", size: 18)
+        enterpriseQuantity.textColor = UIColor.ioasysSearchBarTextColor
+    }
+    
+    func addNotification(){
+        NotificationCenter.default.addObserver(self, selector: #selector(reloadQuantity), name: .ioasysReloadEnterpriseQuantity, object: nil)
+    }
+    
+    private func showEmptyIndication(){
+        enterpriseQuantity.isHidden = true
+        unknownResult.isHidden = false
+        enterpriseTableViewController.view.isHidden = true
+    }
+    
+    private func hideEmptyIndication(){
+        enterpriseQuantity.isHidden = false
+        unknownResult.isHidden = true
+        enterpriseTableViewController.view.isHidden = false
+    }
+    
+    @objc private func reloadQuantity(){
+        if model.enterpriseTotal == 0{
+            showEmptyIndication()
+        } else{
+            hideEmptyIndication()
+        }
+        enterpriseQuantity.text = NSLocalizedString("\(model.enterpriseTotal) resultados encontrados", comment: "")
     }
 }
--- Empresas-ioasys/View/EnterpriseView.swift
+++ Empresas-ioasys/View/EnterpriseView.swift
@@ -34,14 +34,13 @@ class EnterpriseView: UIView{
     public let enterpriseContentView: UIView = EnterpriseContentView()
     public let enterpriseHeader: UIImageView = UIImageView()
     public let enterpriseSearch: UIView = EnterpriseSearchBar()
-    public let enterpriseQuantity: UILabel = UILabel()
 }
 
 extension EnterpriseView: IoasyCustomView{
     
     func autolayout() {
         // enterpriseContentView
-        subviews(enterpriseHeader,enterpriseContentView,enterpriseSearch, enterpriseQuantity)
+        subviews(enterpriseHeader,enterpriseContentView,enterpriseSearch)
             
         enterpriseHeader.Top == safeAreaLayoutGuide.Top
         enterpriseHeader.right(0).left(0).height(148 - 118)
@@ -52,8 +51,6 @@ extension EnterpriseView: IoasyCustomView{
         enterpriseSearch.right(16).left(16).height(48)
         enterpriseSearch.Top == enterpriseHeader.Bottom - 24
         
-        enterpriseQuantity.Top == enterpriseSearch.Bottom + 16
-        enterpriseQuantity.right(16).left(16)
     }
     
     func style() {
@@ -62,20 +59,11 @@ extension EnterpriseView: IoasyCustomView{
         // enterpriseContentView
         enterpriseContentView.backgroundColor = .white
         
-        enterpriseQuantity.numberOfLines = 1
-        enterpriseQuantity.textAlignment = .left
-        enterpriseQuantity.font = UIFont(name: "Rubik-Regular", size: 18)
-        enterpriseQuantity.textColor = UIColor.ioasysSearchBarTextColor
     }
     
     func addNotification(){
-        NotificationCenter.default.addObserver(self, selector: #selector(reloadQuantity), name: .ioasysReloadEnterpriseQuantity, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
-    }
-    
-    @objc private func reloadQuantity(){
-        enterpriseQuantity.text = NSLocalizedString("\(model.enterpriseTotal) resultados encontrados", comment: "")
     }
     
     @objc func keyboardWillShow(_ notification: Notification) {
